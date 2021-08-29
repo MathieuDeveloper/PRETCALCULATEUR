f16d2d289ca09f8814498633c5f1365e2832f766
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-08-30 00:31:04 +0200

    pb with math

--- js/site.js
+++ js/site.js
@@ -18,18 +18,45 @@ function calculus(loanAmount, numberPayments,rate) {
     let interestPayment = 0;
     let principalPayment = 0;
     let monthlyRate = 0;
-    LET eachMonth = {};
+    let totalCost = 0;
+    let totalInterestEnd = 0;
+    let eachMonth = {month:[], payment=0, principal:[], interest:[], totalInterest:[], balance:[], totalInterestEnd=0};
 
     totalMonthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-60));
     //25000 × (5 ÷ 1200) ÷ (1 - (1 + 5 ÷ 1200))^(-60)=471.7808411
-    // taux peridique =(1+taux annuel)^(1/periode par an donc 12) -1
+    eachMonth.payment = totalMonthlyPayment;
+    
+    
+    // taux periodique =(1+taux annuel)^(1/periode par an donc 12) -1
     monthlyRate = ((1 + rate) ^ (1 / 12)) - 1;  // 0.161037
     // montant interets = (montant mensualite*nb mensualite)-capital emprunté
     totalInterestEnd = (totalMonthlyPayment * numberPayments) - loanAmount;
-    /* first Term:  interestPayment=loanAmount*monthlyRate
-                    totalmonthlyPayment-interestPayment
-faire un objet  qui contient tous les Array.
+    eachMonth.totalInterestEnd = totalInterestEnd;
+    totalCost = loanAmount + totalInterestEnd;
+    for (let i = 1; i <= numberPayment; i++) {
+        
+        
+
+    }
+    // formules a verifier:
+    /* first Term:  interestPayment1==loanAmount-monthlyRate
+                    principal1==totalMonthlyPayment-interestPayment1
+                    balance1==loanAmount-principal1
+
+    second Term:    interestPayment2==balance1*monthlyRate
+                    interestPayment2==(loanAmount-principal1)*monthlyRate
+
+                    principal2==totalMonthlyPayment-interestPayment2
+                    balance2==balance1-principal2
+
+
 
 */
+    eachMonth.month.push(69);
+    alert(eachMonth.month);
+    
+}
+
+function display() {
     
 }
