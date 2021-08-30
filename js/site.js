953f23e079fecbfec9d92dcf664952bf030548ed
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-08-30 20:32:24 +0200

    updatingJS MATH

--- js/site.js
+++ js/site.js
@@ -13,25 +13,25 @@ function getValue() {
 }
 // use toFixed() to round and convert to string ! 
 function calculus(loanAmount, numberPayments,rate) {
-    let totalMonthlyPayment = 0;
-    let remainingBalance = 0;
-    let interestPayment = 0;
-    let principalPayment = 0;
-    let monthlyRate = 0;
+    let payment = 0;
+    let balance = 0;
+    let interest = 0;
+    let principal = 0;
+    //let monthlyRate = 0;
     let totalCost = 0;
-    let totalInterestEnd = 0;
-    let eachMonth = {month:[0], payment=0, principal:[0], interest:[0], totalInterest:[0], balance:[loanAmount], totalInterestEnd=0};
-
-    totalMonthlyPayment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-60));
+    let totalInterest = 0;
+    let eachMonth = {month:[0], payment=0, principal:[0], interest:[0], totalInterest:[0], balance:[loanAmount], totalInterest=0};
+//payment est la mensualité
+    payment = (loanAmount) * (rate / 1200) / (1 - (1 + rate / 1200) ** (-60));
     //25000 × (5 ÷ 1200) ÷ (1 - (1 + 5 ÷ 1200))^(-60)=471.7808411
-    eachMonth.payment = totalMonthlyPayment;
+    eachMonth.payment = payment;
     
     
     // taux periodique =(1+taux annuel)^(1/periode par an donc 12) -1
-    monthlyRate = ((1 + rate) ^ (1 / 12)) - 1;  // 0.161037
+    //monthlyRate = ((1 + rate/100) ^ (1 / 12)) - 1;  // 0.00407412378
     // montant interets = (montant mensualite*nb mensualite)-capital emprunté
-    totalInterestEnd = (totalMonthlyPayment * numberPayments) - loanAmount;
-    eachMonth.totalInterestEnd = totalInterestEnd;
+    totalInterest = (payment * numberPayments) - loanAmount;
+    eachMonth.totalInterest = totalInterest;
     totalCost = loanAmount + totalInterestEnd;
     for (let i = 1; i <= numberPayment; i++) {
         
@@ -40,20 +40,16 @@ function calculus(loanAmount, numberPayments,rate) {
     }
     // formules a verifier:
     //balance 0 =loanAmount
-    /* first Term:  interestPayment1==balance0*monthlyRate
-                    principal1==totalMonthlyPayment-interestPayment1
+    /* first Term:  interest1==balance0*5/1200
+                    principal1==payment-interest1
                     balance1==balance0-principal1
 
-                    eachMonth.interest[i]==eachMonth.balance[i-1]*monthlyRate
-                    principali==totalMonthlyPayment-interestPayment1
-                    balancei==balance0-principal1
+                    eachMonth.interest[i]==eachMonth.balance[i-1]*5/1200
+                    principali==payment-interesti
+                    balancei==balance[i-1]]-principali
 
 
-    second Term:    interestPayment2==balance1*monthlyRate
-                    interestPayment2==(loanAmount-principal1)*monthlyRate
-
-                    principal2==totalMonthlyPayment-interestPayment2
-                    balance2==balance1-principal2
+   
 
 
 
